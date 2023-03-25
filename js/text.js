6af23547b6e381a7c59a6562cd751fe32fd31565
AlexLin <alexlinares0209@gmail.com>
2023-03-25 02:09:10 -0500

    agregué js

---
+++ js/text.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".auto-type", {
+    strings: ["La pasión del fútbol.",
+    "Camisetas de fútbol exclusivas.",
+    "Colección de camisetas impresionantes.",
+    "Camisetas de fútbol auténticas."],
+    typeSpeed: 20,
+    backSpeed: 50,
+    loop: true
+})
